fd6c82022c7ee0947054f74aa9fff33f37910a7e
Mikhail Voronin <MVoronin@blondinka.ru>
2014-05-18 01:55:07 +0400

    init

---
+++ RuCaptchaClient/RuCaptchaClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mevoronin.RuCaptchaNETClient
+{
+    public class RuCaptchaClient
+    {
+        private readonly string api_key;
+
+        public RuCaptchaClient(string api_key)
+        {
+            this.api_key = api_key;
+        }
+
+
+    }
+}
